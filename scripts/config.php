86acfda0c443360d47137bbd1552eff70060e164
K L Bhanu <klbm9999@gmail.com>
2018-11-21 18:21:36 +0530

    Remove credentials

--- scripts/config.php
+++ scripts/config.php
@@ -1,10 +1,10 @@
 <?php
 return [
     'host' => 'localhost',
-    'db_name' => 'quiz',
-    'user' => 'root',
+    'db_name' => '',
+    'user' => '',
     'pass' => '',
     'no_of_questions' => 3,
-    'admin_pass' => 'parmanubabu'
+    'admin_pass' => ''
 ];
 ?>
